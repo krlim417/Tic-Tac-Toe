fc07c02b1967af6897b9132e0321a1c26217e727
krlim417 <krlim417@gmail.com>
2018-03-15 13:15:15 -0700

    Handles player turns and marks play on board if valid

--- react-client/src/components/table.jsx
+++ react-client/src/components/table.jsx
@@ -4,7 +4,32 @@ class Table extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      turn: 'x'
+      turn: 'X',
+      topLeft: '',
+      topMid: '',
+      topRight: '',
+      midLeft: '',
+      midCenter: '',
+      midRight: '',
+      botLeft: '',
+      botMid: '',
+      botRight: '',
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    event.preventDefault();
+    const playerTargetSquare = event.target.className;
+    const currentTurn = this.state.turn;
+    if (this.state[playerTargetSquare] === '') {
+      let newTurn = this.state.turn === 'X' ? 'O' : 'X';
+      this.state[playerTargetSquare] = currentTurn;
+      this.setState({
+        turn: newTurn,
+      }, () => {
+        console.log('After', this.state.turn);
+      });
     }
   }
 
@@ -14,19 +39,19 @@ class Table extends React.Component {
         <table>
           <tbody>
             <tr>
-              <td></td>
-              <td></td>
-              <td></td>
+              <td className="topLeft" onClick={this.handleClick}>{this.state.topLeft}</td>
+              <td className='topMid' onClick={this.handleClick}>{this.state.topMid}</td>
+              <td className='topRight' onClick={this.handleClick}>{this.state.topRight}</td>
             </tr>
             <tr>
-              <td></td>
-              <td></td>
-              <td></td>
+              <td className='midLeft' onClick={this.handleClick}>{this.state.midLeft}</td>
+              <td className='midCenter' onClick={this.handleClick}>{this.state.midCenter}</td>
+              <td className='midRight' onClick={this.handleClick}>{this.state.midRight}</td>
             </tr>
             <tr>
-              <td></td>
-              <td></td>
-              <td></td>
+              <td className='botLeft' onClick={this.handleClick}>{this.state.botLeft}</td>
+              <td className='botMid' onClick={this.handleClick}>{this.state.botMid}</td>
+              <td className='botRight' onClick={this.handleClick}>{this.state.botRight}</td>
             </tr>
           </tbody>
         </table>
